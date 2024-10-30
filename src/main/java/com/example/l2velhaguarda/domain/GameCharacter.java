ad345bd1a96842465412747b06e3eabfe18b5d61
Renan Veríssimo <rv.sv@hotmail.com>
2024-10-30 17:34:03 -0300

    feat: line commented

--- src/main/java/com/example/l2velhaguarda/domain/GameCharacter.java
+++ src/main/java/com/example/l2velhaguarda/domain/GameCharacter.java
@@ -11,7 +11,7 @@ import lombok.*;
 public class GameCharacter {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
